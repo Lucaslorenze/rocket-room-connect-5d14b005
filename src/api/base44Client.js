25ea50ffce359f9cfd8db4f00bbac9c4ecec0b0e
Lucaslorenze <Lucas.lorenze@gmail.com>
2025-07-12 18:03:45 -0300

    Add new admin user

--- src/api/base44Client.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@base44/sdk';
-// import { getAccessToken } from '@base44/sdk/utils/auth-utils';
-
-// Create a client with authentication required
-export const base44 = createClient({
-  appId: "686c0802d1cf9db45d14b005", 
-  requiresAuth: true // Ensure authentication is required for all operations
-});
